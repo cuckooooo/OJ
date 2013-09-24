57c44df419ba32d5c328129acbb6dd977c299a74
cuckooooo <maybe.tian@gmail.com>
2013-09-24 19:56:06 +0800

    Javascript_senior_YangHuiSanJiao

---
+++ JAVASCRIPT/senior/�������/selftestcase/self_test_case.js
@@ -0,0 +1,8 @@
+/**
+ * 提供给考生的文件，用于考生自行测试
+ */
+describe("TestSuit", function() {
+	it("test", function() {
+	     expect(3).toBe(cal(4));			
+	});
+});
---
+++ JAVASCRIPT/senior/�������/src/main.js
@@ -0,0 +1,22 @@
+/**
+ * 杨辉三角形，要求输出第n行的最大值，例如第4行最大值是3
+ * 1
+ * 1 1
+ * 1 2 1
+ * 1 3 3 1
+ * 1 4 6 4 1
+ * 1 5 10 10 5 1
+ */
+function cal(n) {	
+	if(0 == n || n < 0)
+	{
+		return 0;
+	}
+	var max_num_of_the_current_line = 1;
+	for(var i = 1; i < n/2; i++)
+	{
+		max_num_of_the_current_line = max_num_of_the_current_line * (n - i) / i;
+	}
+	
+    return max_num_of_the_current_line;
+}
